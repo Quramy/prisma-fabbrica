59018654cfa05f3acfef2761a502d6bbe39697e3
Quramy <yosuke.kurami@gmail.com>
2022-11-18 00:45:48 +0900

    test: Modify sample test case

--- examples/example-prj/src/sample.test.ts
+++ examples/example-prj/src/sample.test.ts
@@ -17,31 +17,23 @@ const PostFactory = defineFactory("Post", {
 const prisma = jestPrisma.client;
 
 describe("factories", () => {
-  describe("user", () => {
-    beforeEach(async () => {
-      await UserFactory.create({ name: "quramy" });
-      await UserFactory.create({ name: "kurami" });
-    });
-    test("created", async () => {
-      prisma.user.create({
-        data: {
-          id: "",
-          name: "",
-          posts: {
-            create: [],
-          },
-        },
-      });
+  describe("UserFactory", () => {
+    it("creates records without input parameters", async () => {
+      await UserFactory.create();
+      await UserFactory.create();
       await expect(prisma.user.count()).resolves.toBe(2);
+    });
+
+    it("creates record with input parameters", async () => {
+      await UserFactory.create({ id: "user001", name: "Quramy" });
+      const user = await prisma.user.findUnique({ where: { id: "user001" } });
+      expect(user).toEqual({ id: "user001", name: "Quramy" });
     });
   });
 
-  describe("post", () => {
-    beforeEach(async () => {
+  describe("PostFactory", () => {
+    it("creates required association", async () => {
       await PostFactory.create();
-    });
-
-    test("post", async () => {
       const created = await prisma.post.findFirst({ include: { author: true } });
       expect(created?.id).not.toBeFalsy();
       expect(created?.author.id).not.toBeFalsy();
