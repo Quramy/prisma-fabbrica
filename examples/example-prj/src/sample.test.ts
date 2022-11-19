60643ae3b5fcf7cb8dc06c4d7b0b63d1e0119f4c
Quramy <yosuke.kurami@gmail.com>
2022-11-20 02:18:41 +0900

    wip: API design proposal

--- examples/example-prj/src/sample.test.ts
+++ examples/example-prj/src/sample.test.ts
@@ -1,17 +1,13 @@
-import { defineUserFactory, definePostFactory } from "./__generated__/fabbrica";
+import { defineUserFactory, definePostFactory } from "./__generated__/fabbrica.mod";
 
 const UserFactory = defineUserFactory({
   defaultData: {},
 });
 
 const PostFactory = definePostFactory({
-  defaultData: async () => ({
-    author: {
-      connect: {
-        id: (await UserFactory.create()).id,
-      },
-    },
-  }),
+  defaultData: {
+    author: UserFactory,
+  },
 });
 
 const prisma = jestPrisma.client;
@@ -37,6 +33,17 @@ describe("factories", () => {
       const created = await prisma.post.findFirst({ include: { author: true } });
       expect(created?.id).not.toBeFalsy();
       expect(created?.author.id).not.toBeFalsy();
+    });
+  });
+
+  describe("PostFactory", () => {
+    it("creates record with association", async () => {
+      const user = await UserFactory.createForConnect({ name: "quramy" });
+      await PostFactory.create({ author: { connect: user } });
+      await PostFactory.create({ author: { connect: user } });
+      await PostFactory.create({ author: { connect: user } });
+      const userWithPosts = await prisma.user.findFirst({ where: { name: "quramy" }, include: { posts: true } });
+      expect(userWithPosts?.posts.length).toBe(3);
     });
   });
 });
