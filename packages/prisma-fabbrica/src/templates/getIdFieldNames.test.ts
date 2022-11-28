66986519695995d7aaba9e37f74d234a23001337
Quramy <yosuke.kurami@gmail.com>
2022-11-29 01:23:37 +0900

    fix: Models without @id nor @@id

---
+++ packages/prisma-fabbrica/src/templates/getIdFieldNames.test.ts
@@ -0,0 +1,43 @@
+import { getDMMF } from "@prisma/internals";
+
+import { modelScalarOrEnumFields, getIdFieldNames } from ".";
+
+describe(modelScalarOrEnumFields, () => {
+  it.each([
+    {
+      pattern: "@id",
+      datamodel: `
+        model TestModel {
+          id Int @id
+        }
+      `,
+      expected: ["id"],
+    },
+    {
+      pattern: "no PK but @unique",
+      datamodel: `
+        model TestModel {
+          field1 Int @unique
+          field2 Int @unique
+        }
+      `,
+      expected: ["field1"],
+    },
+    {
+      pattern: "Complex id",
+      datamodel: `
+        model TestModel {
+          field1 Int
+          field2 String
+          @@id([field1, field2])
+        }
+      `,
+      expected: ["field1", "field2"],
+    },
+  ])("generates literal type field for $pattern", async ({ datamodel, expected }) => {
+    const dmmf = await getDMMF({
+      datamodel,
+    });
+    expect(getIdFieldNames(dmmf.datamodel.models[0])).toEqual(expected);
+  });
+});
