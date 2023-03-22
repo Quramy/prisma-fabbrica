b54d33ef5923ff793afcbe4310f38611ec256462
Quramy <yosuke.kurami@gmail.com>
2023-03-23 01:14:56 +0900

    chore: Trait interface

--- packages/ts-compile-testing/fixtures/simple-model/__generated__/fabbrica/index.ts
+++ packages/ts-compile-testing/fixtures/simple-model/__generated__/fabbrica/index.ts
@@ -25,9 +25,16 @@ type UserFactoryDefineInput = {
 
 type UserFactoryDefineOptions = {
     defaultData?: Resolver<UserFactoryDefineInput, BuildDataOptions>;
+    traits?: {
+      [traitName: string | symbol]: { 
+        data: Resolver<Partial<UserFactoryDefineInput>, BuildDataOptions>;
+      }
+    }
 };
 
-export interface UserFactoryInterface {
+type TTraitKeys<TOptions extends UserFactoryDefineOptions> = keyof TOptions["traits"]
+
+export interface UserFactoryInterfaceWithoutTraits {
     readonly _factoryFor: "User";
     build(inputData?: Partial<Prisma.UserCreateInput>): PromiseLike<Prisma.UserCreateInput>;
     buildCreateInput(inputData?: Partial<Prisma.UserCreateInput>): PromiseLike<Prisma.UserCreateInput>;
@@ -36,6 +43,10 @@ export interface UserFactoryInterface {
     create(inputData?: Partial<Prisma.UserCreateInput>): PromiseLike<User>;
     createList(inputData: number | readonly Partial<Prisma.UserCreateInput>[]): PromiseLike<User[]>;
     createForConnect(inputData?: Partial<Prisma.UserCreateInput>): PromiseLike<Pick<User, "id">>;
+}
+
+export interface UserFactoryInterface<TOptions extends UserFactoryDefineOptions> extends UserFactoryInterfaceWithoutTraits {
+  traits(name: TTraitKeys<TOptions> | readonly TTraitKeys<TOptions>[]): UserFactoryInterfaceWithoutTraits;
 }
 
 function autoGenerateUserScalarsOrEnums({ seq }: {
@@ -47,47 +58,68 @@ function autoGenerateUserScalarsOrEnums({ seq }: {
     };
 }
 
-function defineUserFactoryInternal({ defaultData: defaultDataResolver }: UserFactoryDefineOptions): UserFactoryInterface {
-    const seqKey = {};
-    const getSeq = () => getSequenceCounter(seqKey);
-    const screen = createScreener("User", modelFieldDefinitions);
-    const build = async (inputData: Partial<Prisma.UserCreateInput> = {}) => {
-        const seq = getSeq();
-        const requiredScalarData = autoGenerateUserScalarsOrEnums({ seq });
-        const resolveValue = normalizeResolver<UserFactoryDefineInput, BuildDataOptions>(defaultDataResolver ?? {});
-        const defaultData = await resolveValue({ seq });
-        const defaultAssociations = {};
-        const data: Prisma.UserCreateInput = { ...requiredScalarData, ...defaultData, ...defaultAssociations, ...inputData };
-        return data;
-    };
-    const buildList = (inputData: number | readonly Partial<Prisma.UserCreateInput>[]) => Promise.all(normalizeList(inputData).map(data => build(data)));
-    const pickForConnect = (inputData: User) => ({
-        id: inputData.id
-    });
-    const create = async (inputData: Partial<Prisma.UserCreateInput> = {}) => {
-        const data = await build(inputData).then(screen);
-        return await getClient<PrismaClient>().user.create({ data });
-    };
-    const createList = (inputData: number | readonly Partial<Prisma.UserCreateInput>[]) => Promise.all(normalizeList(inputData).map(data => create(data)));
-    const createForConnect = (inputData: Partial<Prisma.UserCreateInput> = {}) => create(inputData).then(pickForConnect);
+function defineUserFactoryInternal<TOptions extends UserFactoryDefineOptions>({ defaultData: defaultDataResolver, traits: traitsDefs = { }}: TOptions): UserFactoryInterface<TOptions> {
+    const getFactoryWithTraits = (traitKeys: readonly TTraitKeys<TOptions>[] = []) => {
+      const seqKey = {};
+      const getSeq = () => getSequenceCounter(seqKey);
+      const screen = createScreener("User", modelFieldDefinitions);
+      const build = async (inputData: Partial<Prisma.UserCreateInput> = {}) => {
+          const seq = getSeq();
+          const requiredScalarData = autoGenerateUserScalarsOrEnums({ seq });
+          const resolveValue = normalizeResolver<UserFactoryDefineInput, BuildDataOptions>(defaultDataResolver ?? {});
+          const defaultData = await resolveValue({ seq });
+          const defaultAssociations = {};
+          const traitsData = traitKeys.reduce(async (queue, traitKey) => {
+            const acc = await queue
+            const resolveTraitValue = normalizeResolver<Partial<UserFactoryDefineInput>, BuildDataOptions>(traitsDefs[traitKey]?.data ?? {})
+            const traitData = await resolveTraitValue({ seq })
+            return {
+              ...acc,
+              ...traitData,
+            }
+          }, Promise.resolve<Partial<UserFactoryDefineInput>>({}))
+          const data: Prisma.UserCreateInput = { ...requiredScalarData, ...defaultData, ...defaultAssociations, ...traitsData, ...inputData };
+          return data;
+      };
+      const buildList = (inputData: number | readonly Partial<Prisma.UserCreateInput>[]) => Promise.all(normalizeList(inputData).map(data => build(data)));
+      const pickForConnect = (inputData: User) => ({
+          id: inputData.id
+      });
+      const create = async (inputData: Partial<Prisma.UserCreateInput> = {}) => {
+          const data = await build(inputData).then(screen);
+          return await getClient<PrismaClient>().user.create({ data });
+      };
+      const createList = (inputData: number | readonly Partial<Prisma.UserCreateInput>[]) => Promise.all(normalizeList(inputData).map(data => create(data)));
+      const createForConnect = (inputData: Partial<Prisma.UserCreateInput> = {}) => create(inputData).then(pickForConnect);
+      const factory = {
+          _factoryFor: "User" as const,
+          build,
+          buildList,
+          buildCreateInput: build,
+          pickForConnect,
+          create,
+          createList,
+          createForConnect,
+      };
+      return factory
+    }
+    const factory = getFactoryWithTraits()
+    const traits = (name: TTraitKeys<TOptions> | readonly TTraitKeys<TOptions>[]) => {
+      const names = Array.isArray(name) ? [...name] : [name]
+      return getFactoryWithTraits(names)
+    }
     return {
-        _factoryFor: "User" as const,
-        build,
-        buildList,
-        buildCreateInput: build,
-        pickForConnect,
-        create,
-        createList,
-        createForConnect,
-    };
+      ...factory,
+      traits
+    }
 }
 
 /**
  * Define factory for {@link User} model.
  *
  * @param options
- * @returns factory {@link UserFactoryInterface}
+ * @returns factory {@link UserFactoryInterfaceWithoutTraits}
  */
-export function defineUserFactory(options: UserFactoryDefineOptions = {}): UserFactoryInterface {
-    return defineUserFactoryInternal(options);
+export function defineUserFactory<TOptions extends UserFactoryDefineOptions>(options?: TOptions): UserFactoryInterface<TOptions> {
+    return defineUserFactoryInternal(options ?? {});
 }
--- packages/ts-compile-testing/fixtures/simple-model/seed.ts
+++ packages/ts-compile-testing/fixtures/simple-model/seed.ts
@@ -6,11 +6,22 @@ const prisma = new PrismaClient();
 initialize({ prisma });
 
 async function seed() {
-  const UserFactory = defineUserFactory();
+  const UserFactory = defineUserFactory({
+    defaultData: {},
+    traits: {
+      withdrawal: {
+        data: async () => ({
+          name: "***",
+        }),
+      },
+    },
+  });
 
   await UserFactory.create();
   await UserFactory.create({ name: "Alice" });
   await UserFactory.create({ id: "user002", name: "Bob" });
+
+  await UserFactory.traits("withdrawal").build();
 
   console.log(await prisma.user.count()); // 3
 }
