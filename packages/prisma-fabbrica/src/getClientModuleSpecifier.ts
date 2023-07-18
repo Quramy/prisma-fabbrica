45d640bd0756c220b9db241c44eb273bd3c23f57
Quramy <yosuke.kurami@gmail.com>
2023-07-18 18:02:29 +0900

    fix: prismaClientModuleSpecifier for win32

---
+++ packages/prisma-fabbrica/src/getClientModuleSpecifier.ts
@@ -0,0 +1,18 @@
+import _path from "node:path";
+
+const path = _path.posix;
+
+export function getClientModuleSpecifier(clientGeneratorOutputPath: string | undefined, outputDirname: string) {
+  if (!clientGeneratorOutputPath) {
+    return "@prisma/client";
+  }
+  const clientGeneratorOutputPosixPath = clientGeneratorOutputPath.replace(/\\/g, path.sep);
+  const outputPosixDirname = outputDirname.replace(/\\/g, path.sep);
+
+  const prismaClientModuleSpecifier = path
+    .normalize(clientGeneratorOutputPosixPath)
+    .endsWith(["node_modules", "@prisma", "client"].join(path.sep))
+    ? "@prisma/client"
+    : path.normalize("./" + path.relative(outputPosixDirname, clientGeneratorOutputPosixPath));
+  return prismaClientModuleSpecifier;
+}
