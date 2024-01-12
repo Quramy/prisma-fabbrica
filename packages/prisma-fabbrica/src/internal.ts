480314bdd42c3fccaa2dc9d012c81fcbdb1519fa
Quramy <yosuke.kurami@gmail.com>
2024-01-12 22:09:18 +0900

    feat: Implement callback

--- packages/prisma-fabbrica/src/internal.ts
+++ packages/prisma-fabbrica/src/internal.ts
@@ -1,6 +1,6 @@
 export { getClient } from "./clientHolder";
 export { ModelWithFields, createScreener } from "./relations/screen";
-export { Resolver, normalizeResolver, normalizeList, getSequenceCounter } from "./helpers";
+export { Resolver, normalizeResolver, normalizeList, getSequenceCounter, createCallbackChain } from "./helpers";
 export { createInitializer, initialize, resetSequence } from "./initialize";
 export type { InitializeOptions } from "./initialize";
 export {
