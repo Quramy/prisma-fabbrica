899c9c735ab470ead8c6802c4e3dd31d412b7991
Quramy <yosuke.kurami@gmail.com>
2024-01-15 20:23:29 +0900

    feat: Separate PrismaClient instance for each fabbrica file

--- packages/prisma-fabbrica/src/internal.ts
+++ packages/prisma-fabbrica/src/internal.ts
@@ -1,7 +1,7 @@
 export { getClient } from "./clientHolder";
 export { ModelWithFields, createScreener } from "./relations/screen";
 export { Resolver, normalizeResolver, normalizeList, getSequenceCounter } from "./helpers";
-export { initialize, resetSequence } from "./initialize";
+export { createInitializer, initialize, resetSequence } from "./initialize";
 export {
   getScalarFieldValueGenerator,
   registerScalarFieldValueGenerator,
