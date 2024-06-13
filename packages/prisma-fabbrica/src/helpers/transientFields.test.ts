073d0e09ef92f2312e518522bcc654556bc6c039
Quramy <yosuke.kurami@gmail.com>
2024-06-13 22:54:23 +0900

    feat: Implement transient fileds

---
+++ packages/prisma-fabbrica/src/helpers/transientFields.test.ts
@@ -0,0 +1,17 @@
+import { synthesize } from "./transientFields";
+
+describe(synthesize, () => {
+  const defaultValues = {
+    a: 1,
+  };
+
+  test.each([
+    { inputData: undefined, expected: { a: 1 } },
+    { inputData: null, expected: { a: 1 } },
+    { inputData: {}, expected: { a: 1 } },
+    { inputData: { a: 2 }, expected: { a: 2 } },
+    { inputData: { b: 1 }, expected: { a: 1 } },
+  ])("works with $inputData", ({ inputData, expected }) => {
+    expect(synthesize(defaultValues, inputData)).toEqual(expected);
+  });
+});
