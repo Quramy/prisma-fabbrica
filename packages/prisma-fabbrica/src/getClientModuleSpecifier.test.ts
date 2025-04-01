531afa50e505eaf56a908f10a01eb22af32b0fc9
Kyota Fujikura <184185697+kyota-fujikura@users.noreply.github.com>
2025-04-01 09:48:23 +0900

    fix: append "/index.js" to Prisma client import
    This prevents import errors in ES Modules environments with "module: node16".

--- packages/prisma-fabbrica/src/getClientModuleSpecifier.test.ts
+++ packages/prisma-fabbrica/src/getClientModuleSpecifier.test.ts
@@ -5,11 +5,23 @@ describe(getClientModuleSpecifier, () => {
     { outputDirname: "dist", clientGeneratorOutputPath: undefined, expected: "@prisma/client" },
     { outputDirname: "dist", clientGeneratorOutputPath: "node_modules/@prisma/client", expected: "@prisma/client" },
     { outputDirname: "dist", clientGeneratorOutputPath: "node_modules\\@prisma\\client", expected: "@prisma/client" },
-    { outputDirname: "dist", clientGeneratorOutputPath: "prisma-client", expected: "../prisma-client" },
-    { outputDirname: "dist", clientGeneratorOutputPath: "../prisma-client", expected: "../../prisma-client" },
-    { outputDirname: "./dist", clientGeneratorOutputPath: "../prisma-client", expected: "../../prisma-client" },
-    { outputDirname: ".\\dist", clientGeneratorOutputPath: "..\\prisma-client", expected: "../../prisma-client" },
-    { outputDirname: "dist", clientGeneratorOutputPath: "..\\..\\prisma-client", expected: "../../../prisma-client" },
+    { outputDirname: "dist", clientGeneratorOutputPath: "prisma-client", expected: "../prisma-client/index.js" },
+    { outputDirname: "dist", clientGeneratorOutputPath: "../prisma-client", expected: "../../prisma-client/index.js" },
+    {
+      outputDirname: "./dist",
+      clientGeneratorOutputPath: "../prisma-client",
+      expected: "../../prisma-client/index.js",
+    },
+    {
+      outputDirname: ".\\dist",
+      clientGeneratorOutputPath: "..\\prisma-client",
+      expected: "../../prisma-client/index.js",
+    },
+    {
+      outputDirname: "dist",
+      clientGeneratorOutputPath: "..\\..\\prisma-client",
+      expected: "../../../prisma-client/index.js",
+    },
   ])(
     "outputDirname: $outputDirname, clientGeneratorOutputPath: $clientGeneratorOutputPath, expected: $expected",
     ({ outputDirname, clientGeneratorOutputPath, expected }) => {
